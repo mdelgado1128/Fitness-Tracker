eed8270ac92cbc0e97be99ba8b7910e2bb0507ad
Mark Delgado <markdelgado94@gmail.com>
2021-03-22 15:10:18 -0700

    mongodb deployment

--- server.js
+++ server.js
@@ -10,13 +10,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/apiroute.js"));
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
